0074b05a1422abfff2ff7c4d5fb442c9effa2ada
Amen TETTEH <tkamen@outlook.com>
2023-04-08 17:32:32 +0000

    Add schema migration

---
+++ db/migrate/20230408155626_add_group_ref_to_group_expenses.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddGroupRefToGroupExpenses < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :group_expenses, :group, null: false, foreign_key: true
+  end
+end
